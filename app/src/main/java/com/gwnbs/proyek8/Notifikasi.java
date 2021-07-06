49ee864f976d67f223245637dcfa407b191f861b
Daltray Nababan <birwinecheesee@gmail.com>
2021-07-06 20:57:10 +0700

    Initial commit

---
+++ app/src/main/java/com/gwnbs/proyek8/Notifikasi.java
@@ -0,0 +1,31 @@
+package com.gwnbs.proyek8;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class Notifikasi extends BroadcastReceiver {
+
+    public static String NOTIFICATION_ID = "notification-id";
+    public static String NOTIFICATION = "notification";
+
+    public void onReceive(Context context, Intent intent) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification = intent.getParcelableExtra(NOTIFICATION);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel(MainActivity.NOTIFICATION_CHANNEL_ID,
+                    "Pengingat", importance);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
+        if (notificationManager != null) {
+            notificationManager.notify(id, notification);
+        }
+    }
+
+}
